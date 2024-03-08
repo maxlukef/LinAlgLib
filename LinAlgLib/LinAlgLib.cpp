c4c9033660e49c0bbcf76383e0363fd5c831b6bd
maxlukef <u1253508@utah.edu>
2024-03-07 19:11:23 -0700

    Add project files.

---
+++ LinAlgLib/LinAlgLib.cpp
@@ -0,0 +1,12 @@
+﻿// LinAlgLib.cpp : Defines the entry point for the application.
+//
+
+#include "LinAlgLib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
